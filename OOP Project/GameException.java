4e821c34069b465457ae8e82333565f0f4677275
Aiman-005 <manfarooqui004@gmail.com>
2025-06-29 16:44:48 +0500

    Delete OOP Project directory

--- OOP Project/GameException.java
+++
@@ -1,7 +0,0 @@
-package projects;
-
-public class GameException extends Exception {
-    public GameException(String message) {
-        super(message);
-    }
-}
--- OOP Project/IPlayer.java
+++
@@ -1,7 +0,0 @@
-package projects;
-import java.io.Serializable;
-
-public interface IPlayer extends Serializable {
-    int getScore();
-    void setScore(int score);
-}
